1415fcbc8531f818dfe791a4d01d0fd18bf02c7a
Olga Grigashkina <rodakolga@mail.ru>
2024-06-27 13:39:46 +0700

    main

---
+++ main.java
@@ -0,0 +1,26 @@
+import units.*;
+
+public class main {
+    public static void main(String[] args) {
+        
+        Robber robber = new Robber("Serg", 36);
+        Peasant peasant = new Peasant("Max", 86);
+        Sniper sniper = new Sniper("Koly", 45);
+        Wizard wizard = new Wizard("Andrey", 40);
+        Pikeman pikeman = new Pikeman("Gleb", 20);
+        Crossbowman crossbowman = new Crossbowman("Egor", 28);
+        Monk monk = new Monk("Ily", 26);
+
+        
+        System.out.println(robber);
+        System.out.println(peasant);
+        System.out.println(sniper);
+        System.out.println(wizard);
+        System.out.println(pikeman);
+        System.out.println(crossbowman);
+        System.out.println(monk);
+
+    }
+
+    
+}
