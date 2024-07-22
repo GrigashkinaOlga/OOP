b34c52f4b914bfadef7d58abf3d2e15b18ef5962
Olga Grigashkina <rodakolga@mail.ru>
2024-07-22 11:59:51 +0700

    CorXY

--- main.java
+++ main.java
@@ -1,25 +1,67 @@
 import units.*;
+import units.Character;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class main {
+    
+    static ArrayList<Character> red = new ArrayList<>();
+    static ArrayList<Character> blue = new ArrayList<>();
+    
+    
     public static void main(String[] args) {
         
-        Robber robber = new Robber("Serg", 36);
-        Peasant peasant = new Peasant("Max", 86);
-        Sniper sniper = new Sniper("Koly", 45);
-        Wizard wizard = new Wizard("Andrey", 40);
-        Pikeman pikeman = new Pikeman("Gleb", 20);
-        Crossbowman crossbowman = new Crossbowman("Egor", 28);
-        Monk monk = new Monk("Ily", 26);
+        createTeam(red, 10, 0);
+        createTeam(blue, 10, 3);
+        System.out.println();
+        System.out.println(red + "\n");
+        System.out.println(blue);
 
-        
-        System.out.println(robber);
-        System.out.println(peasant);
-        System.out.println(sniper);
-        System.out.println(wizard);
-        System.out.println(pikeman);
-        System.out.println(crossbowman);
-        System.out.println(monk);
+        Sniper sniper = new Sniper("Arny", 7, 4);
+        Character target = sniper.findNearestEnemy(blue);
+        System.out.println("Nearnest blue target at  " + target);
+        target = sniper.findNearestEnemy(red);
+        System.out.println("Nearnest red target at  " + target);
+    
 
+    }
+
+    public static void createTeam(ArrayList<Character> team, int num, int start) {
+        Random rnd = new Random();
+        while (-- num >= 0)
+        {
+            int n = start + rnd.nextInt(4);
+            switch (n)
+            {
+                case 0:
+                    team.add(new Pikeman(getName(), 0, num));
+                    break;
+                case 1:
+                    team.add(new Crossbowman(getName(), 0, num));
+                    break;
+                case 2:
+                    team.add(new Wizard(getName(), 0, num));
+                    break;
+                case 3:
+                    team.add(new Peasant(getName(), start*3, num));
+                    break;
+                case 4:
+                    team.add(new Monk(getName(), 9, num));
+                    break;
+                case 5:
+                    team.add(new Sniper(getName(), 9, num));
+                    break;
+                case 6:
+                    team.add(new Robber(getName(), 9, num));
+                    break;
+
+            }
+        }
+    }
+       
+    private static String getName(){
+        return String.valueOf(Names.values()[new Random().nextInt(Names.values().length)]);
     }
 
     
